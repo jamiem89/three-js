341d462e8f9312da6fff9600e4582f28af3255ad
Jamie Moore <jamiemoore2689@gmail.com>
2024-05-21 16:50:55 +0100

    completed lesson 1-05

--- 01 - Basics/04-transform-objects/src/script.js
+++ 01 - Basics/04-transform-objects/src/script.js
@@ -9,10 +9,36 @@ const scene = new THREE.Scene()
 /**
  * Objects
  */
-const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
-const mesh = new THREE.Mesh(geometry, material)
-scene.add(mesh)
+// const geometry = new THREE.BoxGeometry(1, 1, 1)
+// const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+// const mesh = new THREE.Mesh(geometry, material)
+// // Position
+// mesh.position.x = .6
+// mesh.position.y = -.6
+// mesh.position.z = -.6
+// // Scale
+// mesh.scale.set(1.2, 0.4, .8);
+// // Rotation - Pi = half a rotation;
+// mesh.rotation.x = .5;
+// mesh.rotation.y = Math.PI;
+// mesh.rotation.z = 1;
+// scene.add(mesh)
+
+// Grouping objects
+const group = new THREE.Group();
+const cube1 = new THREE.Mesh(new THREE.BoxGeometry(1, 1, 1), new THREE.MeshBasicMaterial({color: 'red'}));
+const cube2 = new THREE.Mesh(new THREE.BoxGeometry(1, 1, 1), new THREE.MeshBasicMaterial({color: 'green'}));
+cube2.position.x = 1.2
+const cube3 = new THREE.Mesh(new THREE.BoxGeometry(1, 1, 1), new THREE.MeshBasicMaterial({color: 'blue'}));
+cube3.position.x = -1.2
+group.add(cube1);
+group.add(cube2);
+group.add(cube3);
+scene.add(group);
+
+// Create axes helper - property is length of lines
+const axesHelper = new THREE.AxesHelper(2);
+scene.add(axesHelper);
 
 /**
  * Sizes
@@ -27,7 +53,9 @@ const sizes = {
  */
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
 camera.position.z = 3
-scene.add(camera)
+scene.add(camera);
+// We can use lookAt to look at the centre of an object
+// camera.lookAt(mesh.position)
 
 /**
  * Renderer
